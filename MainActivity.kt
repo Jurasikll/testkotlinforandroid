37a8bb280cd27428dbb3014d52305ac60ff2f1fd
Jurasikll <Jurasikll@yandex.ru>
2018-07-11 00:05:09 +0300

    0.0.2
    Clean

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,7 +1,6 @@
 package com.jurasik.ll.testkotlinforandroid
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.*
 import android.os.Bundle
 
 
@@ -10,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener { doSomething() }
     }
 
 }
